fdede6aeda0bd4110c7ba24e85b872223c6ea4b8
synacker <milovidovmikhail@gmail.com>
2019-11-29 13:21:47 +0300

    Added read rights for service under LocalService

--- register_log.cmd
+++ register_log.cmd
@@ -6,6 +6,8 @@ pushd "%~dp0"
 set manifest=%~dp0manifest.man
 set resource=%~dp0service_win.exe
 
+icacls %resource% /grant "NT AUTHORITY\LocalService":R /Q
+
 wevtutil.exe im "%manifest%" /rf:"%resource%" /mf:"%resource%" /pf:"%resource%"
 wevtutil.exe sl "service_win_log/log" /ab:true /rt:true /ms:4194304
 wevtutil.exe sl "service_win_log/audit" /ab:true /rt:true /ms:4194304
